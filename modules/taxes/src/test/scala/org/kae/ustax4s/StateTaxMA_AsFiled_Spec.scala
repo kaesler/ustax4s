a0081d26fd446b737f577dc38a1becdffd06a92b
Kevin Esler <kevin.esler@gmail.com>
2020-12-04 12:37:53 -0500

    Added test to compare with my 2019

--- modules/taxes/src/test/scala/org/kae/ustax4s/StateTaxMA_AsFiled_Spec.scala
+++ modules/taxes/src/test/scala/org/kae/ustax4s/StateTaxMA_AsFiled_Spec.scala
@@ -5,15 +5,15 @@ import org.kae.ustax4s.FilingStatus.HeadOfHousehold
 import org.specs2.mutable.Specification
 
 object StateTaxMA_AsFiled_Spec extends Specification with IntMoneySyntax {
+
   "MA State tax" >> {
     "should match what I filed in 2018" >> {
 
       val incomeBeforeExemptions =
-        // Wages etc, line 10
+        // Wages etc, line 18
         122210.tm +
           // interest and dividends
           8958.tm +
-
           // Line 2g
           7800.tm
       StateTaxMA.taxDue(
@@ -24,5 +24,23 @@ object StateTaxMA_AsFiled_Spec extends Specification with IntMoneySyntax {
         incomeBeforeExemptions
       ).rounded === 6690.tm
     }
+  }
+
+  "should match what I filed in 2019" >> {
+
+      val incomeBeforeExemptions =
+        // Wages etc, line 10
+        148032.tm +
+          // interest and dividends
+          7776.tm +
+          // Line 2g
+          7800.tm
+      StateTaxMA.taxDue(
+        Year.of(2018),
+        HeadOfHousehold,
+        Kevin.birthDate,
+        1)(
+        incomeBeforeExemptions
+      ).rounded === 7918.tm
   }
 }
