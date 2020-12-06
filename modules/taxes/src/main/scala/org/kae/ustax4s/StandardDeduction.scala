bfb9b8e8b5b8432f5ee0ae5f7819e3efb22ed1ee
Kevin Esler <kevin.esler@gmail.com>
2020-12-06 18:48:50 -0500

    Add some Single tax rate details

--- modules/taxes/src/main/scala/org/kae/ustax4s/StandardDeduction.scala
+++ modules/taxes/src/main/scala/org/kae/ustax4s/StandardDeduction.scala
@@ -1,13 +1,13 @@
 package org.kae.ustax4s
 
 import java.time.{LocalDate, Month, Year}
-import org.kae.ustax4s.FilingStatus.HeadOfHousehold
+import org.kae.ustax4s.FilingStatus.{HeadOfHousehold, Single}
 
 object StandardDeduction {
 
   def of(year: Year, filingStatus: FilingStatus, birthDate: LocalDate): TMoney =
     unadjustedForAge(year, filingStatus) +
-      (if (isAge65OrOlder(birthDate, year)) TMoney.u(1300) else TMoney.zero)
+      (if (isAge65OrOlder(birthDate, year)) TMoney.u(1350) else TMoney.zero)
 
   def unadjustedForAge(year: Year, filingStatus: FilingStatus): TMoney =
     (year.getValue, filingStatus) match {
@@ -15,6 +15,11 @@ object StandardDeduction {
       case (2020, HeadOfHousehold) => TMoney.u(18650)
       case (2019, HeadOfHousehold) => TMoney.u(18350)
       case (2018, HeadOfHousehold) => TMoney.u(18000)
+
+      case (2021, Single) => TMoney.u(12550)
+      case (2020, Single) => TMoney.u(12400)
+      case (2019, Single) => TMoney.u(12200)
+      case (2018, Single) => TMoney.u(12000)
       case _ => ???
     }
 
