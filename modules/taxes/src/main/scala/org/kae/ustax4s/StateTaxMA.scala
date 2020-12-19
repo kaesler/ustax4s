14a6d5cf3770bdfd8226be11ffc37fc8293e7f6f
Kevin Esler <kevin.esler@gmail.com>
2020-12-19 13:44:31 -0500

    Extend MA standard exemptions to currently non-covered years.

--- modules/taxes/src/main/scala/org/kae/ustax4s/StateTaxMA.scala
+++ modules/taxes/src/main/scala/org/kae/ustax4s/StateTaxMA.scala
@@ -1,7 +1,7 @@
 package org.kae.ustax4s
 
 import java.time.{LocalDate, Year}
-import org.kae.ustax4s.FilingStatus.HeadOfHousehold
+import org.kae.ustax4s.FilingStatus.{HeadOfHousehold, Single}
 
 object StateTaxMA extends IntMoneySyntax {
 
@@ -17,8 +17,19 @@ object StateTaxMA extends IntMoneySyntax {
   ): TMoney = {
     val unadjustedForAge = (year.getValue, filingStatus) match {
       case (2020, HeadOfHousehold) => TMoney.u(6800)
+      case (2020, Single) => TMoney.u(4400)
+
       case (2019, HeadOfHousehold) => TMoney.u(6800)
+      case (2019, Single) => TMoney.u(4400)
+
       case (2018, HeadOfHousehold) => TMoney.u(6800)
+      case (2018, Single) => TMoney.u(4400)
+
+      // TODO: for now assume same for future years as 2020.
+      case (year, fs) if year > 2020 =>
+        personalExemption(Year.of(2020), fs, birthDate)
+      // Fail for inapplicable past years.
+      case (year, _) if year < 2018 => ???
     }
 
     unadjustedForAge + (if (isAge65OrOlder(birthDate, year)) 700.tm
