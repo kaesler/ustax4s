274362b0e7dd095069d2350dc0d70958099c33db
Kevin Esler <kevin.esler@gmail.com>
2021-05-16 19:33:40 -0400

    More MUnit

--- modules/taxes/src/test/scala/org/kae/ustax4s/StateTaxMA_AsFiled_Spec.scala
+++ modules/taxes/src/test/scala/org/kae/ustax4s/StateTaxMA_AsFiled_Spec.scala
@@ -1,46 +1,46 @@
 package org.kae.ustax4s
 
 import java.time.Year
+import munit.ScalaCheckSuite
 import org.kae.ustax4s.FilingStatus.HeadOfHousehold
-import org.specs2.mutable.Specification
 
-object StateTaxMA_AsFiled_Spec extends Specification with IntMoneySyntax {
+class StateTaxMA_AsFiled_Spec extends ScalaCheckSuite with IntMoneySyntax {
 
-  "MA State tax" >> {
-    "should match what I filed in 2018" >> {
+  test("MA State tax should match what I filed in 2018") {
 
-      val incomeBeforeExemptions =
-        // Line 19: 5.1% income after deductions
-        122210.tm +
-          // Line 20: interest and dividends
-          8958.tm +
-          // Line 2g: total exemptions (these get removed in calcs)
-          7800.tm
-      StateTaxMA.taxDue(
-        Year.of(2018),
-        HeadOfHousehold,
-        Kevin.birthDate,
-        1)(
-        incomeBeforeExemptions
-      ).rounded === 6690.tm
-    }
+    val incomeBeforeExemptions =
+      // Line 19: 5.1% income after deductions
+      122210.tm +
+        // Line 20: interest and dividends
+        8958.tm +
+        // Line 2g: total exemptions (these get removed in calcs)
+        7800.tm
+
+    assertEquals(
+      StateTaxMA
+        .taxDue(Year.of(2018), HeadOfHousehold, Kevin.birthDate, 1)(
+          incomeBeforeExemptions
+        )
+        .rounded,
+      6690.tm
+    )
   }
 
-  "should match what I filed in 2019" >> {
-
-      val incomeBeforeExemptions =
-        // Wages etc, line 10
-        148032.tm +
-          // interest and dividends
-          7776.tm +
-          // Line 2g
-          7800.tm
-      StateTaxMA.taxDue(
-        Year.of(2018),
-        HeadOfHousehold,
-        Kevin.birthDate,
-        1)(
-        incomeBeforeExemptions
-      ).rounded === 7918.tm
+  test("MA State tax should match what I filed in 2019") {
+    val incomeBeforeExemptions =
+      // Wages etc, line 10
+      148032.tm +
+        // interest and dividends
+        7776.tm +
+        // Line 2g
+        7800.tm
+    assertEquals(
+      StateTaxMA
+        .taxDue(Year.of(2018), HeadOfHousehold, Kevin.birthDate, 1)(
+          incomeBeforeExemptions
+        )
+        .rounded,
+      7918.tm
+    )
   }
 }
