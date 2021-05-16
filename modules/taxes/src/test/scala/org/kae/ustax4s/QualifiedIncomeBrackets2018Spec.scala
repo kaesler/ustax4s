dc923a7881f4a425cd6d624670189a1655e3a807
Kevin Esler <kevin.esler@gmail.com>
2021-05-16 16:58:36 -0400

    More MUnit

--- modules/taxes/src/test/scala/org/kae/ustax4s/QualifiedIncomeBrackets2018Spec.scala
+++ modules/taxes/src/test/scala/org/kae/ustax4s/QualifiedIncomeBrackets2018Spec.scala
@@ -1,20 +1,23 @@
 package org.kae.ustax4s
 
 import java.time.Year
+import munit.FunSuite
 import org.kae.ustax4s.FilingStatus.HeadOfHousehold
-import org.specs2.mutable.Specification
 
-object QualifiedIncomeBrackets2018Spec extends Specification {
+class QualifiedIncomeBrackets2018Spec extends FunSuite {
 
-  "InvestmentIncomeTaxBrackets for HOH 2018" >> {
-    "should match my actual return" >> {
-      val brackets = QualifiedIncomeBrackets.of(Year.of(2018), HeadOfHousehold)
+  test(
+    "QualifiedIncomeTaxBrackets for HOH 2018 should match my actual return"
+  ) {
+    val brackets = QualifiedIncomeBrackets.of(Year.of(2018), HeadOfHousehold)
+    assertEquals(
       brackets
         .taxDueFunctionally(
           TMoney.u(114547),
           TMoney.u(14777)
         )
-        .rounded === TMoney.u(2217)
-    }
+        .rounded,
+      TMoney.u(2217)
+    )
   }
 }
