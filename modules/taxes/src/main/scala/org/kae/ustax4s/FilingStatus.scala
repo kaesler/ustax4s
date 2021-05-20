22a267975795f00ab87831cab9aa0de52d14efc6
Kevin Esler <kevin.esler@gmail.com>
2021-05-20 12:05:09 -0400

    Typo

--- modules/taxes/src/main/scala/org/kae/ustax4s/FilingStatus.scala
+++ modules/taxes/src/main/scala/org/kae/ustax4s/FilingStatus.scala
@@ -2,4 +2,4 @@ package org.kae.ustax4s
 
 enum FilingStatus(val entryName: String):
   case Single extends FilingStatus("Single")
-  case HeadOfHousehold extends FilingStatus("HeadofHousehold")
+  case HeadOfHousehold extends FilingStatus("HeadOfHousehold")
