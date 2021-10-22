98a5d0830b3afe47afcea0509cf8a20bcde58b45
Kevin Esler <kevin.esler@gmail.com>
2021-10-22 18:25:56 -0400

    Delete overly complicated way of doing inflation, in favor of DI-by-currying.

--- modules/taxes/src/main/scala/org/kae/ustax4s/federal/newcalculator/Person.scala
+++
@@ -1,9 +0,0 @@
-package org.kae.ustax4s.federal.newcalculator
-
-import org.kae.ustax4s.FilingStatus
-import java.time.LocalDate
-
-final case class Person(
-  birthDate: LocalDate,
-  personalExemptions: Int
-)
