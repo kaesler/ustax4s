d9a3102f8d75e1bd2c687864d4775dff742d76c6
Kevin Esler <kevin.esler@gmail.com>
2021-12-19 15:16:10 -0500

    Provide FederalTaxRate.zero

--- modules/taxes/src/main/scala/org/kae/ustax4s/federal/FederalTaxRate.scala
+++ modules/taxes/src/main/scala/org/kae/ustax4s/federal/FederalTaxRate.scala
@@ -9,13 +9,20 @@ opaque type FederalTaxRate = Double
 
 object FederalTaxRate:
 
+  val zero: FederalTaxRate = 0.0
+
   def unsafeFrom(d: Double): FederalTaxRate =
     require(d >= 0.0)
     require(d <= 0.40)
     d
 
   given tr: TaxRate[FederalTaxRate] with
-    extension (r: FederalTaxRate) def asFraction = r
+    extension (left: FederalTaxRate)
+      def asFraction: Double = left
+
+      infix def absoluteDifference(right: FederalTaxRate): FederalTaxRate =
+        (left - right).abs
+    end extension
 
   given Ordering[FederalTaxRate] with
     def compare(x: FederalTaxRate, y: FederalTaxRate): Int =
