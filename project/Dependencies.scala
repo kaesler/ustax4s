a7995b70a7967891e174c6fe198a8c682735e25c
Kevin Esler <kevin.esler@gmail.com>
2022-01-29 12:30:34 -0500

    Updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   object Cats {
     val core: ModuleID   = "org.typelevel" %% "cats-core"   % "2.7.0"
-    val effect: ModuleID = "org.typelevel" %% "cats-effect" % "3.3.4"
+    val effect: ModuleID = "org.typelevel" %% "cats-effect" % "3.3.5"
   }
 
   object MUnit {
