cb7eb230916586e31dce901d5f5a5800d59ed6ce
Kevin Esler <kevin.esler@gmail.com>
2022-07-20 12:09:21 -0400

    Beefed up a test

--- modules/ustax4s/src/test/scala/org/kae/ustax4s/federal/BoundRegimeFutureEstimationSpec.scala
+++ modules/ustax4s/src/test/scala/org/kae/ustax4s/federal/BoundRegimeFutureEstimationSpec.scala
@@ -1,5 +1,6 @@
 package org.kae.ustax4s.federal
 
+import cats.implicits.*
 import java.time.{LocalDate, Year}
 import munit.FunSuite
 import org.kae.ustax4s.FilingStatus.HeadOfHousehold
@@ -22,14 +23,12 @@ class BoundRegimeFutureEstimationSpec extends FunSuite:
     val estimate = InflationEstimate(Year.of(2027), 0.03)
 
     val after = before.futureEstimated(estimate)
-    assert(
-      before.standardDeduction < after.standardDeduction
-    )
-    assert(
-      before.netDeduction(4000) < after.netDeduction(4000)
-    )
+    assert(before.standardDeduction < after.standardDeduction)
+    assert(before.netDeduction(4000) < after.netDeduction(4000))
     assert(
       after.perPersonExemption == (before.perPersonExemption inflateBy estimate.factor(baseYear))
     )
+    assert(before.ordinaryBrackets <= after.ordinaryBrackets)
+    assert(before.qualifiedBrackets <= after.qualifiedBrackets)
   }
 end BoundRegimeFutureEstimationSpec
