c7571357ae9530ae1e89874136edf1a2651eaf11
Kevin Esler <kevin.esler@gmail.com>
2022-07-21 09:33:04 -0400

    Prop test

--- modules/ustax4s/src/test/scala/org/kae/ustax4s/federal/BoundRegimeFutureEstimationSpec.scala
+++ modules/ustax4s/src/test/scala/org/kae/ustax4s/federal/BoundRegimeFutureEstimationSpec.scala
@@ -3,8 +3,9 @@ package org.kae.ustax4s.federal
 import cats.implicits.*
 import java.time.{LocalDate, Year}
 import munit.{FunSuite, ScalaCheckSuite}
-import org.kae.ustax4s.FilingStatus.HeadOfHousehold
-import org.kae.ustax4s.InflationEstimate
+import org.kae.ustax4s.FilingStatus
+import org.kae.ustax4s.federal.yearly.YearlyValues
+import org.kae.ustax4s.{FilingStatus, InflationEstimate}
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Arbitrary, Gen}
 
@@ -21,13 +22,43 @@ class BoundRegimeFutureEstimationSpec extends ScalaCheckSuite:
   // TODO add monotonicity prop spec for taxes due.
   //  - vary year, inflation, estimate, regime, income details
 
+  property("BoundRegime.forFutureYear behaves as expected") {
+    forAll { (tc: TestCase2) =>
+      val after = BoundRegime.forFutureYear(
+        tc.regime,
+        tc.futureYear,
+        tc.inflationFactorEstimate,
+        birthDate,
+        tc.filingStatus,
+        tc.personalExemptions
+      )
+
+      val before = BoundRegime.forKnownYear(
+        YearlyValues.lastFor(tc.regime).year,
+        birthDate,
+        tc.filingStatus,
+        tc.personalExemptions
+      )
+
+      val res =
+        (before.standardDeduction <= after.standardDeduction) &&
+          (before.netDeduction(0) <= after.netDeduction(0)) &&
+          (before.ordinaryBrackets <= after.ordinaryBrackets) &&
+          (before.qualifiedBrackets <= after.qualifiedBrackets)
+      if !res then {
+        println(before.show)
+        println(after.show)
+      }
+      res
+    }
+  }
   property("BoundRegime.withEstimatedNetInflationFactor behaves as expected") {
     forAll { (tc: TestCase1) =>
       val before = BoundRegime.forKnownYear(
         tc.baseYear,
         birthDate,
-        HeadOfHousehold,
-        2
+        tc.filingStatus,
+        tc.personalExemptions
       )
 
       val after = before.withEstimatedNetInflationFactor(
@@ -53,7 +84,9 @@ object BoundRegimeFutureEstimationSpec:
   private final case class TestCase1(
     baseYear: Year,
     futureYear: Year,
-    inflationFactorEstimate: Double
+    inflationFactorEstimate: Double,
+    filingStatus: FilingStatus,
+    personalExemptions: Int
   )
 
   private given Arbitrary[TestCase1] = Arbitrary(
@@ -61,6 +94,37 @@ object BoundRegimeFutureEstimationSpec:
       baseYear                <- Gen.choose(2017, 2022).map(Year.of)
       futureYear              <- Gen.choose(2023, 2055).map(Year.of)
       inflationFactorEstimate <- Gen.choose(1.005, 1.10)
-    } yield TestCase1(baseYear, futureYear, inflationFactorEstimate)
+      filingStatus            <- Gen.oneOf(FilingStatus.values.toList)
+      personalExemptions      <- Gen.choose(0, 4)
+    } yield TestCase1(
+      baseYear,
+      futureYear,
+      inflationFactorEstimate,
+      filingStatus,
+      personalExemptions
+    )
+  )
+  private final case class TestCase2(
+    regime: Regime,
+    futureYear: Year,
+    inflationFactorEstimate: Double,
+    filingStatus: FilingStatus,
+    personalExemptions: Int
+  )
+
+  private given Arbitrary[TestCase2] = Arbitrary(
+    for {
+      regime                  <- Gen.oneOf(Regime.values.toList)
+      futureYear              <- Gen.choose(2023, 2055).map(Year.of)
+      inflationFactorEstimate <- Gen.choose(1.005, 1.10)
+      filingStatus            <- Gen.oneOf(FilingStatus.values.toList)
+      personalExemptions      <- Gen.choose(0, 4)
+    } yield TestCase2(
+      regime,
+      futureYear,
+      inflationFactorEstimate,
+      filingStatus,
+      personalExemptions
+    )
   )
 end BoundRegimeFutureEstimationSpec
