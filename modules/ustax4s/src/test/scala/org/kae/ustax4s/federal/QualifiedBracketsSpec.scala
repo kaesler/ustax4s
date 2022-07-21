00ecdeb788f199f8bee19ba56648580c2c9d0113
Kevin Esler <kevin.esler@gmail.com>
2022-07-21 09:13:56 -0400

    Dead code

--- modules/ustax4s/src/test/scala/org/kae/ustax4s/federal/QualifiedBracketsSpec.scala
+++ modules/ustax4s/src/test/scala/org/kae/ustax4s/federal/QualifiedBracketsSpec.scala
@@ -15,7 +15,6 @@ class QualifiedBracketsSpec
     with QualifiedBracketsGeneration
     with MoneyGeneration:
 
-  // import math.Ordered.orderingToOrdered
   import TaxFunctions.*
   import math.Ordering.Implicits.infixOrderingOps
 
