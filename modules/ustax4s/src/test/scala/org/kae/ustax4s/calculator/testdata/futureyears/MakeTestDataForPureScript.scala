81d45277ecdb77067e2cf88b240e499fc29e806c
Kevin Esler <kevin.esler@gmail.com>
2022-07-27 14:27:49 -0400

    MakeTestDataForTypeScript

---
+++ modules/ustax4s/src/test/scala/org/kae/ustax4s/calculator/testdata/futureyears/MakeTestDataForPureScript.scala
@@ -0,0 +1,42 @@
+package org.kae.ustax4s.calculator.testdata.futureyears
+
+import org.kae.ustax4s.calculator.TaxCalculator
+
+object MakeTestDataForPureScript extends App:
+  import TestDataGeneration.*
+
+  testCases.foreach {
+    case tc @ TestCaseInputs(regime, year, estimate, bd, fs, deps, ss, oi, qi, itm) =>
+      val federalTaxDue = TaxCalculator.federalTaxDueForFutureYear(
+        regime = regime,
+        futureYear = year,
+        estimatedAnnualInflationFactor = estimate,
+        birthDate = bd,
+        filingStatus = fs,
+        tc.personalExemptions,
+        socSec = ss,
+        ordinaryIncomeNonSS = oi,
+        qualifiedIncome = qi,
+        itemizedDeductions = itm
+      )
+
+      val stateTaxDue = TaxCalculator.stateTaxDue(
+        year = year,
+        birthDate = bd,
+        filingStatus = fs,
+        dependents = deps,
+        massachusettsGrossIncome = oi + qi
+      )
+      val bdRep   = s"(unsafeMakeDate ${bd.getYear} ${bd.getMonthValue} ${bd.getDayOfMonth})"
+      val yearRep = s"(unsafeMakeYear ${year.getValue})"
+      println(
+        s"  TestCase { regime: $regime, year: $yearRep, " +
+          s"estimatedAnnualInflationFactor: $estimate, birthDate: $bdRep, " +
+          s"personalExemptions: ${tc.personalExemptions}, filingStatus: $fs, " +
+          s"socSec: makeFromInt $ss, ordinaryIncomeNonSS: makeFromInt $oi, " +
+          s"qualifiedIncome: makeFromInt $qi, " +
+          s"itemizedDeductions: makeFromInt $itm, " +
+          s"federalTaxDue: makeFromInt $federalTaxDue, stateTaxDue: makeFromInt" +
+          s" $stateTaxDue },"
+      )
+  }
