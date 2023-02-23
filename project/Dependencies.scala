246f2a9f63f66f5612c536dc818e863ace8c8078
Kevin Esler <kevin.esler@gmail.com>
2023-02-23 07:12:48 -0500

    Update libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ import sbt._
 object Dependencies {
 
   object Cats {
-    val core: ModuleID   = "org.typelevel" %% "cats-core"   % "2.8.0"
-    val effect: ModuleID = "org.typelevel" %% "cats-effect" % "3.3.14"
+    val core: ModuleID   = "org.typelevel" %% "cats-core"   % "2.9.0"
+    val effect: ModuleID = "org.typelevel" %% "cats-effect" % "3.4.8"
   }
 
   object MUnit {
