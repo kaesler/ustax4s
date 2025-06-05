00f52a5f854f9dde448b982c9f7ebb3733549925
Kevin Esler <kevin.esler@gmail.com>
2025-06-05 10:52:29 -0400

    Corrected comments so we get online help in a GSheet

--- modules/gsheetfacade/src/main/scala/org/kae/ustax4s/gsheetfacade/Facade.scala
+++ modules/gsheetfacade/src/main/scala/org/kae/ustax4s/gsheetfacade/Facade.scala
@@ -13,14 +13,10 @@ object Facade:
   /** Standard deduction for a known year. Example: TIR_STD_DEDUCTION(2022, "HeadOfHousehold",
     * 1955-10-02)
     *
-    * @param {number}
-    *   year the tax regime to use, one of "TCJA", "PreTCJA"
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {object}
-    *   birthDate tax payer's date of birth
-    * @returns
-    *   {number} The standard deduction
+    * @param {number} year the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {object} birthDate tax payer's date of birth
+    * @returns {number} The standard deduction
     */
   @JSExportTopLevel("tir_std_deduction")
   def tir_std_deduction(
@@ -36,18 +32,12 @@ object Facade:
   /** Standard deduction for a future year. Example: TIR_FUTURE_STD_DEDUCTION("TCJA", 3%, 2030,
     * "HeadOfHousehold", 1955-10-02)
     *
-    * @param {string}
-    *   regime the tax regime to use, one of "TCJA", "PreTCJA"
-    * @param {number}
-    *   bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
-    * @param {number}
-    *   year a year in the future, after the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {object}
-    *   birthDate tax payer's date of birth
-    * @returns
-    *   {number} The standard deduction
+    * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
+    * @param {number} year a year in the future, after the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {object} birthDate tax payer's date of birth
+    * @returns {number} The standard deduction
     */
   @JSExportTopLevel("tir_future_std_deduction")
   def tir_future_std_deduction(
@@ -70,14 +60,10 @@ object Facade:
   /** Width of an ordinary income tax bracket for a known year. Example:
     * TIR_ORDINARY_BRACKET_WIDTH(2022, "Single", 10)
     *
-    * @param {number}
-    *   year a year between 2016 and the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {number}
-    *   ordinaryRatePercentage rate for a tax bracket e.g. 22
-    * @returns
-    *   {number} The width of the specified ordinary income tax bracket.
+    * @param {number} year a year between 2016 and the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {number} ordinaryRatePercentage rate for a tax bracket e.g. 22
+    * @returns {number} The width of the specified ordinary income tax bracket.
     */
   @JSExportTopLevel("tir_ordinary_bracket_width")
   def tir_ordinary_bracket_width(
@@ -94,14 +80,10 @@ object Facade:
   /** End of an ordinary income tax bracket for a known year. Example:
     * TIR_ORDINARY_BRACKET_END(2022, "Single", 10)
     *
-    * @param {number}
-    *   year a year between 2016 and the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {number}
-    *   ordinaryRatePercentage rate for a tax bracket e.g. 22
-    * @returns
-    *   {number} The end of the specified ordinary bracket
+    * @param {number} year a year between 2016 and the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {number} ordinaryRatePercentage rate for a tax bracket e.g. 22
+    * @returns {number} The end of the specified ordinary bracket
     */
   @JSExportTopLevel("tir_ordinary_bracket_end")
   def tir_ordinary_bracket_end(
@@ -118,18 +100,12 @@ object Facade:
   /** Width of an ordinary income tax bracket for a future year. Example:
     * TIR_FUTURE_ORDINARY_BRACKET_WIDTH("PreTCJA", 2030, "HeadOfHousehold", 10)
     *
-    * @param {string}
-    *   regime the tax regime to use, one of "TCJA", "PreTCJA"
-    * @param {number}
-    *   bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
-    * @param {number}
-    *   year a year in the future, after the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {number}
-    *   ordinaryRatePercentage rate for a tax bracket e.g. 22
-    * @returns
-    *   {number} The width of the specified ordinary income bracket.
+    * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
+    * @param {number} year a year in the future, after the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {number} ordinaryRatePercentage rate for a tax bracket e.g. 22
+    * @returns {number} The width of the specified ordinary income bracket.
     */
   @JSExportTopLevel("tir_future_ordinary_bracket_width")
   def tir_future_ordinary_bracket_width(
@@ -153,18 +129,12 @@ object Facade:
   /** End of an ordinary income tax bracket for a future year. Example:
     * TIR_FUTURE_ORDINARY_BRACKET_END("PreTCJA", 2030, "HeadOfHousehold", 10)
     *
-    * @param {string}
-    *   regime the tax regime to use, one of "TCJA", "PreTCJA"
-    * @param {number}
-    *   bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
-    * @param {number}
-    *   year a year in the future, after the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {number}
-    *   ordinaryRatePercentage rate for a tax bracket e.g. 22
-    * @returns
-    *   {number} The end of the specified ordinary income bracket.
+    * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
+    * @param {number} year a year in the future, after the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {number} ordinaryRatePercentage rate for a tax bracket e.g. 22
+    * @returns {number} The end of the specified ordinary income bracket.
     */
   @JSExportTopLevel("tir_future_ordinary_bracket_end")
   def tir_future_ordinary_bracket_end(
@@ -188,12 +158,9 @@ object Facade:
   /** Threshold above which long term capital gains are taxed, for a known year. Example:
     * TIR_LTCG_TAX_START(2022, "HeadOfHousehold")
     *
-    * @param {number}
-    *   year a year between 2016 and the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @returns
-    *   {number} the end of the zero tax rate on qualified investment income
+    * @param {number} year a year between 2016 and the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @returns {number} the end of the zero tax rate on qualified investment income
     */
   @JSExportTopLevel("tir_ltcg_tax_start")
   def tir_ltcg_tax_start(
@@ -209,16 +176,11 @@ object Facade:
   /** Threshold above which long term capital gains are taxed, for a future year Example:
     * TIR_FUTURE_LTCG_TAX_START("PreTCJA", 2027, 3.4%, "HeadOfHousehold")
     *
-    * @param {string}
-    *   regime the tax regime to use, one of "TCJA", "PreTCJA"
-    * @param {number}
-    *   year a year in the future, after the current year
-    * @param {number}
-    *   bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @returns
-    *   {number} the end of the zero tax rate on qualified investment income
+    * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {number} year a year in the future, after the current year
+    * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @returns {number} the end of the zero tax rate on qualified investment income
     */
   @JSExportTopLevel("tir_future_ltcg_tax_start")
   def tir_future_ltcg_tax_start(
@@ -235,10 +197,8 @@ object Facade:
 
   /** The RMD fraction for a given age. Example: TIR_RMD_FRACTION_FOR_AGE(76)
     *
-    * @param {number}
-    *   age age of the taxpayer
-    * @returns
-    *   {number} the RMD fraction
+    * @param {number} age age of the taxpayer
+    * @returns {number} the RMD fraction
     */
   @JSExportTopLevel("tir_rmd_fraction_for_age")
   def tir_rmd_fraction_for_age(
@@ -249,24 +209,15 @@ object Facade:
   /** The Federal tax due for a known year. Example: TIR_FEDERAL_TAX_DUE(2022, "Single", 1955-10-02,
     * 0, 10000, 40000, 5000, 0)
     *
-    * @param {number}
-    *   year a year between 2016 and the current year
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {object}
-    *   birthDate tax payer's date of birth
-    * @param {number}
-    *   personalExemptions self plus dependents, only relevant in a PreTCJA year
-    * @param {number}
-    *   socSec Social Security benefits received
-    * @param {number}
-    *   ordinaryIncomeNonSS ordinary income excluding Social Security
-    * @param {number}
-    *   qualifiedIncome qualified dividends and long term capital gains
-    * @param {number}
-    *   itemizedDeductions total of any itemized deductions
-    * @returns
-    *   {number} the Federal tax due
+    * @param {number} year a year between 2016 and the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {object} birthDate tax payer's date of birth
+    * @param {number} personalExemptions self plus dependents, only relevant in a PreTCJA year
+    * @param {number} socSec Social Security benefits received
+    * @param {number} ordinaryIncomeNonSS ordinary income excluding Social Security
+    * @param {number} qualifiedIncome qualified dividends and long term capital gains
+    * @param {number} itemizedDeductions total of any itemized deductions
+    * @returns {number} the Federal tax due
     */
   @JSExportTopLevel("tir_federal_tax_due")
   def tir_federal_tax_due(
@@ -296,28 +247,17 @@ object Facade:
   /** The Federal tax due for a future year. Example: TIR_FUTURE_FEDERAL_TAX_DUE("TCJA", 2023,
     * 0.034, "Single", 1955-10-02, 0, 10000, 40000, 5000, 0)
     *
-    * @param {string}
-    *   regime the tax regime to use, one of "TCJA", "PreTCJA"
-    * @param {number}
-    *   year a year in the future, after the current year
-    * @param {number}
-    *   bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
-    * @param {string}
-    *   filingStatus one of "Single", "HeadOfHousehold", "Married"
-    * @param {object}
-    *   birthDate tax payer's date of birth
-    * @param {number}
-    *   personalExemptions self plus dependents, only relevant in a PreTCJA year
-    * @param {number}
-    *   socSec Social Security benefits received
-    * @param {number}
-    *   ordinaryIncomeNonSS ordinary income excluding Social Security
-    * @param {number}
-    *   qualifiedIncome qualified dividends and long term capital gains
-    * @param {number}
-    *   itemizedDeductions total of any itemized deductions
-    * @returns
-    *   {number} the Federal tax due
+    * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {number} year a year in the future, after the current year
+    * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {object} birthDate tax payer's date of birth
+    * @param {number} personalExemptions self plus dependents, only relevant in a PreTCJA year
+    * @param {number} socSec Social Security benefits received
+    * @param {number} ordinaryIncomeNonSS ordinary income excluding Social Security
+    * @param {number} qualifiedIncome qualified dividends and long term capital gains
+    * @param {number} itemizedDeductions total of any itemized deductions
+    * @returns {number} the Federal tax due
     */
   @JSExportTopLevel("tir_future_federal_tax_due")
   def tir_future_federal_tax_due(
@@ -346,132 +286,38 @@ object Facade:
       .taxDue
   end tir_future_federal_tax_due
 
-//  /**
-//   * The marginal tax rate on ordinary income for a known year.
-//   * Example: TIR_FEDERAL_TAX_SLOPE(2022, "Single", 1955-10-02, 0, 10000, 40000, 5000, 0, 1000)
-//   *
-//   * @param {number} year a year between 2016 and the current year
-//   * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
-//   * @param {object} birthDate tax payer's date of birth
-//   * @param {number} personalExemptions self plus dependents, only relevant in a PreTCJA year
-//   * @param {number} socSec Social Security benefits received
-//   * @param {number} ordinaryIncomeNonSS  ordinary income excluding Social Security
-//   * @param {number} qualifiedIncome qualified dividends and long term capital gains
-//   * @param {number} itemizedDeductions total of any itemized deductions
-//   * @param {number} ordinaryIncomeDelta the change to ordinaryIncomeNonSS with which to calculate a slope
-//   * @returns {number} the marginal tax rate as a percentage.
-//   * @customfunction
-//   */
-//  function TIR_FEDERAL_TAX_SLOPE (
-//    year,
-//    filingStatus,
-//    birthDate,
-//    personalExemptions,
-//    socSec,
-//    ordinaryIncomeNonSS,
-//    qualifiedIncome,
-//    itemizedDeductions,
-//    ordinaryIncomeDelta
-//  ) {
-//    const start = Math.min(
-//      ordinaryIncomeNonSS,
-//      ordinaryIncomeNonSS + ordinaryIncomeDelta)
-//    const end = Math.max(
-//      ordinaryIncomeNonSS,
-//      ordinaryIncomeNonSS + ordinaryIncomeDelta)
-//
-//    const federalTaxAtStart = TIR_FEDERAL_TAX_DUE(
-//      year,
-//      filingStatus,
-//      birthDate,
-//      personalExemptions,
-//      socSec,
-//      start,
-//      qualifiedIncome,
-//      itemizedDeductions
-//    );
-//    const federalTaxAtEnd = TIR_FEDERAL_TAX_DUE(
-//      year,
-//      filingStatus,
-//      birthDate,
-//      personalExemptions,
-//      socSec,
-//      end,
-//      qualifiedIncome,
-//      itemizedDeductions
-//    );
-//    const deltaY = (federalTaxAtEnd - federalTaxAtStart);
-//
-//    return deltaY / Math.abs(ordinaryIncomeDelta);
-//  }
-//
-//  /**
-//   * The marginal tax rate on ordinary income for a future year.
-//   * Example: TIR_FUTURE_FEDERAL_TAX_SLOPE("TCJA", 2023, 0.034, "Single", 1955-10-02, 0, 10000, 40000, 5000, 0, 1000)
-//   *
-//   * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
-//   * @param {number} year a year in the future, after the current year
-//   * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
-//   * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
-//   * @param {object} birthDate tax payer's date of birth
-//   * @param {number} personalExemptions self plus dependents, only relevant in a PreTCJA year
-//   * @param {number} socSec Social Security benefits received
-//   * @param {number} ordinaryIncomeNonSS  ordinary income excluding Social Security
-//   * @param {number} qualifiedIncome qualified dividends and long term capital gains
-//   * @param {number} itemizedDeductions total of any itemized deductions
-//   * @param {number} ordinaryIncomeDelta the change to ordinaryIncomeNonSS with which to calculate a slope
-//   * @returns {number} the marginal tax rate as a percentage.
-//   * @customfunction
-//   */
-//  function TIR_FUTURE_FEDERAL_TAX_SLOPE (
-//    regime,
-//    year,
-//    bracketInflationRate,
-//    filingStatus,
-//    birthDate,
-//    personalExemptions,
-//    socSec,
-//    ordinaryIncomeNonSS,
-//    qualifiedIncome,
-//    itemizedDeductions,
-//    ordinaryIncomeDelta
-//  ) {
-//    const start = Math.min(
-//      ordinaryIncomeNonSS,
-//      ordinaryIncomeNonSS + ordinaryIncomeDelta)
-//    const end = Math.max(
-//      ordinaryIncomeNonSS,
-//      ordinaryIncomeNonSS + ordinaryIncomeDelta)
-//
-//    const federalTaxAtStart = TIR_FUTURE_FEDERAL_TAX_DUE(
-//      regime,
-//      year,
-//      bracketInflationRate,
-//      filingStatus,
-//      birthDate,
-//      personalExemptions,
-//      socSec,
-//      start,
-//      qualifiedIncome,
-//      itemizedDeductions
-//    );
-//    const federalTaxAtEnd = TIR_FUTURE_FEDERAL_TAX_DUE(
-//      regime,
-//      year,
-//      bracketInflationRate,
-//      filingStatus,
-//      birthDate,
-//      personalExemptions,
-//      socSec,
-//      end,
-//      qualifiedIncome,
-//      itemizedDeductions
-//    );
-//    const deltaY = (federalTaxAtEnd - federalTaxAtStart);
-//
-//    return deltaY / Math.abs(ordinaryIncomeDelta);
-//  }
-//
+  /** The marginal tax rate on ordinary income for a known year. Example:
+    * TIR_FEDERAL_TAX_SLOPE(2022, "Single", 1955-10-02, 0, 10000, 40000, 5000, 0, 1000)
+    *
+    * @param {number} year a year between 2016 and the current year
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {object} birthDate tax payer's date of birth
+    * @param {number} personalExemptions self plus dependents, only relevant in a PreTCJA year
+    * @param {number} socSec Social Security benefits received
+    * @param {number} ordinaryIncomeNonSS ordinary income excluding Social Security
+    * @param {number} qualifiedIncome qualified dividends and long term capital gains
+    * @param {number} itemizedDeductions total of any itemized deductions
+    * @param {number} ordinaryIncomeDelta the change to ordinaryIncomeNonSS with which to calculate a slope
+    * @returns {number} the marginal tax rate as a percentage.
+    */
+
+  /** The marginal tax rate on ordinary income for a future year. Example:
+    * TIR_FUTURE_FEDERAL_TAX_SLOPE("TCJA", 2023, 0.034, "Single", 1955-10-02, 0, 10000, 40000, 5000,
+    * 0, 1000)
+    *
+    * @param {string} regime the tax regime to use, one of "TCJA", "PreTCJA"
+    * @param {number} year a year in the future, after the current year
+    * @param {number} bracketInflationRate estimate of future tax bracket inflation, e.g. 2%
+    * @param {string} filingStatus one of "Single", "HeadOfHousehold", "Married"
+    * @param {object} birthDate tax payer's date of birth
+    * @param {number} personalExemptions self plus dependents, only relevant in a PreTCJA year
+    * @param {number} socSec Social Security benefits received
+    * @param {number} ordinaryIncomeNonSS ordinary income excluding Social Security
+    * @param {number} qualifiedIncome qualified dividends and long term capital gains
+    * @param {number} itemizedDeductions total of any itemized deductions
+    * @param {number} ordinaryIncomeDelta the change to ordinaryIncomeNonSS with which to calculate a slope
+    * @returns {number} the marginal tax rate as a percentage.
+    */
 //  /**
 //   * The amount of Social Security income that is taxable.
 //   * Example: TIR_TAXABLE_SOCIAL_SECURITY("HeadOfHousehold", 20000, 52000)
