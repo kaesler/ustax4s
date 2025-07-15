f4e1ab78cc2ea15b25f60fdab6a311c079d94b4e
Kevin Esler <kevin.esler@gmail.com>
2025-07-15 17:04:58 -0400

    Checkin progress on tests

--- modules/ustax4s/shared/src/main/scala/org/kae/ustax4s/federal/FederalCalcResults.scala
+++ modules/ustax4s/shared/src/main/scala/org/kae/ustax4s/federal/FederalCalcResults.scala
@@ -29,16 +29,17 @@ object FederalCalcResults:
     import r.*
     // b.append(s"  ssRelevantOtherIncome: $ssRelevantOtherIncome\n")
     b.append(s"  taxableSocSec: $taxableSocialSecurity\n")
-    b.append(s"  personalExceptionDeduction: $personalExemptionDeduction\n")
-    // b.append(s"  unadjustedStandardDeduction: $unadjustedStandardDeduction\n")
-    // b.append(s"  adjustmentWhenOver65: $adjustmentWhenOver65\n")
-    // b.append(s"  adjustmentWhenOldAndSingle: $adjustmentWhenOldAndSingle\n")
+    b.append(s"  agi: $agi\n")
+    b.append(s"  personalExemptionDeduction: $personalExemptionDeduction\n")
     b.append(s"  standardDeduction: $standardDeduction\n")
+    b.append(s"  meansTestedSeniorDeduction: $meansTestedSeniorDeduction\n")
     b.append(s"  netDeduction: $netDeduction\n")
     b.append(s"  taxableOrdinaryIncome: $taxableOrdinaryIncome\n")
     b.append(s"  taxOnOrdinaryIncome: $taxOnOrdinaryIncome\n")
     b.append(s"  taxOnQualifiedIncome: $taxOnQualifiedIncome\n")
     b.append(s"  taxDue: $taxDue\n")
+    b.append(s"  taxSlopeForOrdinaryIncome: $taxSlopeForOrdinaryIncome\n")
+    b.append(s"  taxSlopeForQualifiedIncome: $taxSlopeForQualifiedIncome\n")
 
     b.result
 end FederalCalcResults
---
+++ modules/ustax4s/shared/src/test/scala/org/kae/ustax4s/federal/MeansTestedSeniorDeductionSpec.scala
@@ -0,0 +1,230 @@
+package org.kae.ustax4s.federal
+
+import cats.syntax.all.*
+import java.time.{LocalDate, Year}
+import munit.ScalaCheckSuite
+import org.kae.ustax4s.FilingStatus.{MarriedJoint, Single}
+import org.kae.ustax4s.{Age, FilingStatus}
+import org.kae.ustax4s.calculator.TaxCalculator
+import org.kae.ustax4s.money.Moneys.{Deduction, Income, TaxableIncome}
+import org.scalacheck.Prop.{forAll, propBoolean}
+import org.scalacheck.{Arbitrary, Gen}
+import scala.math.Ordered.orderingToOrdered
+
+class MeansTestedSeniorDeductionSpec extends ScalaCheckSuite:
+  import MeansTestedSeniorDeductionSpec.{*, given}
+
+  // TODO:
+  //  zero unless
+  //    in the years in effect
+  //    over 65
+  //  amount is full below phaseout start
+  //  amount is zero above phaseout
+  //  amount is < 6k otherwise
+
+  property("meansTestedSeniorDeduction is only non-zero 2025-28") {
+    // val birthDate = birthDateUnder65
+    forAll { (sc: Scenario) =>
+      import sc.*
+      val res = TaxCalculator.federalTaxResultsForAnyYear(
+        1.03,
+        year,
+        filingStatus,
+        birthDate,
+        personalExemptions,
+        socSec,
+        ordinaryIncomeNonSS,
+        qualifiedIncome,
+        itemizedDeductions
+      )
+      //  zero unless
+      //    in the years in effect
+      //    over 65
+      res.meansTestedSeniorDeduction == Deduction.zero ||
+      (Set(2025, 2026, 2027, 2028).contains(sc.year.getValue) &&
+        Age.isAge65OrOlder(birthDate, year))
+    }
+  }
+
+  property("meansTestedSeniorDeduction allowed in full for singles") {
+    val birthDateOver65 = LocalDate.of(1955, 10, 2)
+    forAll { (scenario: Scenario) =>
+      val sc = scenario.copy(
+        year = Year.of(2028),
+        birthDate = birthDateOver65,
+        filingStatus = Single
+      )
+      import sc.*
+      val res = TaxCalculator.federalTaxResultsForAnyYear(
+        1.03,
+        year,
+        filingStatus,
+        birthDate,
+        personalExemptions,
+        socSec,
+        ordinaryIncomeNonSS,
+        qualifiedIncome,
+        itemizedDeductions
+      )
+
+      val success = res.agi > Income(75000) ||
+        res.meansTestedSeniorDeduction == Deduction(6000)
+      if !success then println(res.show)
+      success
+    }
+  }
+
+  property("meansTestedSeniorDeduction allowed in full for marrieds") {
+    val birthDateOver65 = LocalDate.of(1955, 10, 2)
+    forAll { (scenario: Scenario) =>
+      val sc = scenario.copy(
+        year = Year.of(2028),
+        birthDate = birthDateOver65,
+        filingStatus = MarriedJoint
+      )
+      import sc.*
+      val res = TaxCalculator.federalTaxResultsForAnyYear(
+        1.03,
+        year,
+        filingStatus,
+        birthDate,
+        personalExemptions,
+        socSec,
+        ordinaryIncomeNonSS,
+        qualifiedIncome,
+        itemizedDeductions
+      )
+
+      val success = res.agi > Income(150000) ||
+        res.meansTestedSeniorDeduction == Deduction(6000)
+      if !success then println(res.show)
+      success
+    }
+  }
+
+  property("meansTestedSeniorDeduction phases out for singles") {
+    val birthDateOver65 = LocalDate.of(1955, 10, 2)
+    forAll { (scenario: Scenario) =>
+      val sc = scenario.copy(
+        year = Year.of(2028),
+        birthDate = birthDateOver65,
+        filingStatus = Single
+      )
+      import sc.*
+      val res = TaxCalculator.federalTaxResultsForAnyYear(
+        1.03,
+        year,
+        filingStatus,
+        birthDate,
+        personalExemptions,
+        socSec,
+        ordinaryIncomeNonSS,
+        qualifiedIncome,
+        itemizedDeductions
+      )
+
+      val success = res.agi < Income(175000) ||
+        res.meansTestedSeniorDeduction == Deduction(0)
+      if !success then println(res.show)
+      success
+    }
+  }
+
+  property("meansTestedSeniorDeduction phases out for marrieds") {
+    val birthDateOver65 = LocalDate.of(1955, 10, 2)
+    forAll { (scenario: Scenario) =>
+      val sc = scenario.copy(
+        year = Year.of(2028),
+        birthDate = birthDateOver65,
+        filingStatus = MarriedJoint
+      )
+      import sc.*
+      val res = TaxCalculator.federalTaxResultsForAnyYear(
+        1.03,
+        year,
+        filingStatus,
+        birthDate,
+        personalExemptions,
+        socSec,
+        ordinaryIncomeNonSS,
+        qualifiedIncome,
+        itemizedDeductions
+      )
+
+      val success = res.agi < Income(275000) ||
+        res.meansTestedSeniorDeduction == Deduction(0)
+      if !success then println(res.show)
+      success
+    }
+  }
+
+//  property("meansTestedSeniorDeduction allowed in part for singles") {
+//    val birthDateOver65 = LocalDate.of(1955, 10, 2)
+//    forAll { (scenario: Scenario) =>
+//      val sc = scenario.copy(
+//        year = Year.of(2028),
+//        birthDate = birthDateOver65,
+//        filingStatus = Single
+//      )
+//      import sc.*
+//      val res = TaxCalculator.federalTaxResultsForAnyYear(
+//        1.03,
+//        year,
+//        filingStatus,
+//        birthDate,
+//        personalExemptions,
+//        socSec,
+//        ordinaryIncomeNonSS,
+//        qualifiedIncome,
+//        itemizedDeductions
+//      )
+//
+//      println(res.show)
+//      val ded = res.meansTestedSeniorDeduction
+//      (ded > Deduction.zero && ded < Deduction(6000)) ==>
+//        res.agi > Income(75000) && res.agi < Income(175000)
+//    }
+//  }
+
+end MeansTestedSeniorDeductionSpec
+
+object MeansTestedSeniorDeductionSpec:
+
+  final case class Scenario(
+    year: Year,
+    filingStatus: FilingStatus,
+    birthDate: LocalDate,
+    // Self plus dependents
+    personalExemptions: Int,
+    socSec: Income,
+    ordinaryIncomeNonSS: Income,
+    qualifiedIncome: TaxableIncome,
+    itemizedDeductions: Deduction
+  )
+
+  private given Arbitrary[Scenario] =
+    Arbitrary(
+      for
+        year         <- Gen.choose(2025, 2035).map(Year.of)
+        filingStatus <- Gen.oneOf(FilingStatus.values.toList)
+        birthDate    <- Gen.oneOf(
+          LocalDate.of(1955, 10, 2),
+          LocalDate.now().minusYears(50)
+        )
+        socSec              <- Gen.chooseNum(0, 60000).map(Income.apply)
+        ordinaryIncomeNonSS <- Gen.chooseNum(0, 150000).map(Income.apply)
+        qualifiedIncome     <- Gen.chooseNum(0, 100000).map(TaxableIncome.apply)
+        itemizedDeductions  <- Gen.chooseNum(0, 30000).map(Deduction.apply)
+      yield Scenario(
+        year,
+        filingStatus,
+        birthDate,
+        personalExemptions = 0,
+        socSec,
+        ordinaryIncomeNonSS,
+        qualifiedIncome,
+        itemizedDeductions
+      )
+    )
+  end given
+end MeansTestedSeniorDeductionSpec
