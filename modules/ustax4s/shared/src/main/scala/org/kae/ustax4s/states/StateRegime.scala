4e0c92ef8167f4839322c6b70548da44bc6fe022
Kevin Esler <kevin.esler@gmail.com>
2025-11-11 15:02:24 -0500

    Initial refactoring for States tax support

--- modules/ustax4s/shared/src/main/scala/org/kae/ustax4s/states/StateRegime.scala
+++ modules/ustax4s/shared/src/main/scala/org/kae/ustax4s/states/StateRegime.scala
@@ -13,3 +13,63 @@ case class FlatStateRegime(
 case class ProgressStateRegime(
   brackets: Brackets[StateTaxRate]
 ) extends StateRegime
+
+object StateRegime:
+  import State.*
+  def of(state: State): StateRegime =
+    state match
+      case AK | FL | NV | NH | SD | TN | TX | WA | WY => NilStateRegime
+
+      case CO => FlatStateRegime(StateTaxRate.unsafeFrom(4.4 / 100))
+      case IL => FlatStateRegime(StateTaxRate.unsafeFrom(4.95 / 100))
+      case IN => FlatStateRegime(StateTaxRate.unsafeFrom(3.05 / 100))
+      case MI => FlatStateRegime(StateTaxRate.unsafeFrom(4.25 / 100))
+      case NC => FlatStateRegime(StateTaxRate.unsafeFrom(4.25 / 100))
+      case PA => FlatStateRegime(StateTaxRate.unsafeFrom(3.07 / 100))
+      case UT => FlatStateRegime(StateTaxRate.unsafeFrom(4.85 / 100))
+
+      // Progressive: TODO
+      case AL => ???
+      case AZ => ???
+      case AR => ???
+      case AS => ???
+      case CA => ???
+      case CT => ???
+      case DE => ???
+      case DC => ???
+      case GA => ???
+      case GU => ???
+      case HI => ???
+      case ID => ???
+      case IA => ???
+      case KS => ???
+      case KY => ???
+      case LA => ???
+      case ME => ???
+      case MD => ???
+      case MA => ???
+      case MN => ???
+      case MS => ???
+      case MO => ???
+      case MT => ???
+      case NE => ???
+      case NJ => ???
+      case NM => ???
+      case NY => ???
+      case ND => ???
+      case MP => ???
+      case OH => ???
+      case OK => ???
+      case OR => ???
+      case PR => ???
+      case RI => ???
+      case SC => ???
+      case TT => ???
+      case VT => ???
+      case VA => ???
+      case VI => ???
+      case WV => ???
+      case WI => ???
+    end match
+  end of
+end StateRegime
