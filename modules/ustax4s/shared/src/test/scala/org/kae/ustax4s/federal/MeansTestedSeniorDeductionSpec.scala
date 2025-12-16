9632adc1f56b0bafc81017d1dda11f1bdbb393e5
Kevin Esler <kevin.esler@gmail.com>
2025-12-16 16:35:23 -0500

    Dead TODO

--- modules/ustax4s/shared/src/test/scala/org/kae/ustax4s/federal/MeansTestedSeniorDeductionSpec.scala
+++ modules/ustax4s/shared/src/test/scala/org/kae/ustax4s/federal/MeansTestedSeniorDeductionSpec.scala
@@ -12,8 +12,6 @@ import org.scalacheck.Prop.{forAll, propBoolean}
 import org.scalacheck.{Arbitrary, Gen}
 import scala.math.Ordered.orderingToOrdered
 
-// TODO: verify married gets twice single
-
 class MeansTestedSeniorDeductionSpec extends ScalaCheckSuite:
   import MeansTestedSeniorDeductionSpec.{*, given}
 
