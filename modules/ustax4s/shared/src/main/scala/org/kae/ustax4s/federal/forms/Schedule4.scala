3e3978aea3e2a7cbf5ba6beddd8060197f8f7d27
Kevin Esler <kevin.esler@gmail.com>
2026-01-29 14:25:08 -0500

    Dead code

--- modules/ustax4s/shared/src/main/scala/org/kae/ustax4s/federal/forms/Schedule4.scala
+++
@@ -1,9 +0,0 @@
-package org.kae.ustax4s.federal.forms
-
-import org.kae.ustax4s.money.TaxPayable
-
-case class Schedule4(
-  selfEmploymentTax: TaxPayable
-):
-  def totalOtherTaxes: TaxPayable = selfEmploymentTax
-end Schedule4
--- modules/ustax4s/shared/src/main/scala/org/kae/ustax4s/federal/forms/Schedule5.scala
+++
@@ -1,8 +0,0 @@
-package org.kae.ustax4s.federal.forms
-
-import org.kae.ustax4s.money.TaxCredit
-
-case class Schedule5(
-  // Line 72:
-  excessSocialSecurityWithheld: TaxCredit
-)
